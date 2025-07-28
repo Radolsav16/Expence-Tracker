33da5b17e05a7dcc45690eba069cf1a051c03d18
Radoslav Todorov <todorovradoslav18@gmail.com>
2025-07-28 22:15:53 +0300

    init base functionality for taking form data in sign-up

--- client/src/pages/Auth/SignUp.tsx
+++ client/src/pages/Auth/SignUp.tsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useState, useTransition } from "react";
 import AuthLayout from "../../components/layouts/AuthLayout";
 import AuthButton from "./components/AuthButton";
 import AuthText from "./components/AuthText";
 import Input from "./components/Input";
 import PhotoInput from "./components/PhotoInput";
+import {useForm} from 'react-hook-form'
 
-
-const SignUp:React.FC = () =>{
   const signUpInputs = [
     {label:"Full Name",type:"text",name:"name",placeholder:"Jane Smith"},
     {label:"Email",type:"email",name:"email",placeholder:"jane@smith.com"},
     {label:"Password",type:"password",name:"password",placeholder:""},
-    {label:"Confirm Password",type:"password",name:"repeat-assword",placeholder:""},
-
-
+    {label:"Confirm Password",type:"password",name:"repeatPassword",placeholder:""},
   ]
+
+
+const SignUp:React.FC = () =>{
+
+   const {isPending,startTransition} = useTransition()
+  
+   const [data,setData] = useState({
+    name:'',
+    email:'',
+    password:'',
+    repeatPassword:'',
+    imageUrl:''
+   })
+
+   const onChangeHandler = (e) =>{
+    console.log('change')
+    console.log(e.currentTarget)
+      setData({
+        ...data,
+        [e.currentTarget.name]:e.currentTarget.value,
+      })
+   }
+
+   const onSubmit = (e) =>{
+    console.log('submitted')
+    const data = new FormData(e.currentTarget)
+    console.log(data)
+   }
 
   return (
   <AuthLayout>
@@ -22,8 +47,8 @@ const SignUp:React.FC = () =>{
   <div className="w-full max-w-md">
     <h2 className="text-2xl font-semibold text-center mb-6">Create Account</h2>
     <PhotoInput />
-    <form className="space-y-5">
-      {signUpInputs.map(o => <Input key={o.name} label={o.label} type={o.type} name={o.name} placeholder={o.placeholder}/>)}
+    <form className="space-y-5" action={onSubmit}>
+      {signUpInputs.map(o => <Input key={o.name} label={o.label} type={o.type} name={o.name} placeholder={o.placeholder} onChangeHandler={onChangeHandler} value={data[o.name]}/>)}
     <AuthButton  label={"Sign Up"}/>
     </form>
     <AuthText text={'Already have an account?'} actionText={'Log in'} path={'/login'} />
--- client/src/pages/Auth/components/Input.tsx
+++ client/src/pages/Auth/components/Input.tsx
@@ -1,4 +1,4 @@
-const Input:React.FC = ({label,type,name,placeholder}) =>{
+const Input:React.FC = ({label,type,name,placeholder,onChangeHandler,value}) =>{
     return(
          <div>
                 <label htmlFor={name} className="block text-sm font-medium text-gray-700 mb-1">
@@ -7,7 +7,10 @@ const Input:React.FC = ({label,type,name,placeholder}) =>{
                 <input
                   id={name}
                   type={type}
+                  name={name}
                   placeholder={placeholder}
+                  onChange={onChangeHandler}
+                  value={value}
                   required
                   className="w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 focus:outline-none"
                 />
